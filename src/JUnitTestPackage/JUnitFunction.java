0792c153f246a768fb162c56835c5951ef618364
stark0dh <dhanajaymourya531@gmail.com>
2022-11-21 09:27:26 +0530

    JUNitTestCase

---
+++ src/JUnitTestPackage/JUnitFunction.java
@@ -0,0 +1,23 @@
+package JUnitTestPackage;
+import java.util.*;
+public class JUnitFunction {
+	public static int Addnumbers(int a ,int b) {
+		int c = a + b ;
+		return c ;
+	}
+	
+	public static String AddString(String a , String b) {
+		String  c = a.concat(b);
+		return c ;
+		
+	}
+	public static void main(String[] args) {
+		int a = 8 ;
+		int b = 5 ;
+		System.out.println(Addnumbers(a,b));
+		String c = "Hello";
+		String d = "World";
+		System.out.println(AddString(c,d));
+	}
+
+}
